ae7ee8642438a466e36a863b5d62b114147c5a1e
Sergey <one011001@gmail.com>
2021-03-30 21:20:01 +0300

    v1.1

--- NeuralNetwork/Forms/FormLearningPerceptron.cs
+++ NeuralNetwork/Forms/FormLearningPerceptron.cs
@@ -5,7 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Linq;
 using NeuralNetwork.Neural;
+
 
 
 namespace NeuralNetwork.Forms
@@ -15,12 +17,33 @@ namespace NeuralNetwork.Forms
     {
         public FormLearningPerceptron()
         {
+            perceptron = new MultiLayerPerceptron(2, 2, 3, 3);
             InitializeComponent();
         }
 
         private void buttonCustomValues_Click(object sender, EventArgs e)
         {
-            
+            //текст преобразуем в массив чисел
+            double[] values = textBox1.Text.Split(' ').Select(x => double.Parse(x)).ToArray();
+            //массив чисел преобразуем во входной вектор
+            Vector Input = new Vector(values.Length);
+            for(int i = 0; i < values.Length; i++)
+            {
+                Input[i] = values[i];
+            }
+
+            //Делаем прямой проход сети
+            Vector output = perceptron.Forward(Input);
+
+            //Это для вывода ответа
+            StringBuilder sbuilder = new StringBuilder();
+            for(int i = 0; i < output.M; i++)
+            {
+                sbuilder.Append(output[i] + " ");
+            }
+
+            MessageBox.Show(sbuilder.ToString());
+
         }
 
 
--- NeuralNetwork/Program.cs
+++ NeuralNetwork/Program.cs
@@ -17,13 +17,13 @@ namespace NeuralNetwork
         [STAThread]
         static void Main()
         {
-            //Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormMain());
-            
-            
-           
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+
+
+
         }
     }
 }
